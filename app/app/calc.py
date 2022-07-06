99aec9edae0db27f803bede3f660828c8a6b8876
Jasmeet Singh <mailtojasmeet@gmail.com>
2022-07-06 22:27:24 +0530

    docker added postgres n test using TDD

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+# calculation function
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+    
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""sample tests"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(51, 40)
+
+        self.assertEqual(res, 11)
