c4c122acd367a13797ce8bff11b536efe44f07ca
Jasmeet Singh <mailtojasmeet@gmail.com>
2022-07-09 11:19:48 +0530

    setup django admin completed

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,48 @@
-# from django.contrib import admin
+"""Django admin customizitions"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+# this is call our custom user model rather
+# than inbuild Django UserAdmin which will create a conflict
+class UserAdmin(BaseUserAdmin):
+    """Defin the admin page for the users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            # classes used html classed declared in django
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+# to register for visibility in django Admin
+admin.site.register(models.User, UserAdmin)
